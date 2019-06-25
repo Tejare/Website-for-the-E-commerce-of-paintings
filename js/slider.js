d217d4ba01d88504a3ce31576e501a2be82f15ca
vineetpardhi <51925185+vineetpardhi@users.noreply.github.com>
2019-06-25 20:27:47 +0530

    update for scripts

---
+++ js/slider.js
@@ -0,0 +1,13 @@
+/*$(document).ready(function(){
+
+  $(".slide1 img").mouseover(function()
+  {
+  $(".slide1 img").css('transform','scale(1.1)');
+
+  });
+
+  $(".slide1 img").mouseout(function()
+  {
+  $(".slide1 img").css('transform','scale(1.0)');
+  });
+});*/
